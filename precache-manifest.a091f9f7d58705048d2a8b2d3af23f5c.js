796d32393ee7f29be5033854ae3bd7db11ec5b2b
pauluzo <45356331+pauluzo@users.noreply.github.com>
2019-11-19 01:49:53 +0100

    Updates

---
+++ precache-manifest.a091f9f7d58705048d2a8b2d3af23f5c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad69bcc21fc4a34b49cb619687b7414b",
+    "url": "/nubieducation/index.html"
+  },
+  {
+    "revision": "87b4adb70682c785d755",
+    "url": "/nubieducation/static/css/main.aff60b2b.chunk.css"
+  },
+  {
+    "revision": "82e15d7a9edb999a883c",
+    "url": "/nubieducation/static/js/2.c5f2e226.chunk.js"
+  },
+  {
+    "revision": "87b4adb70682c785d755",
+    "url": "/nubieducation/static/js/main.86412ba0.chunk.js"
+  },
+  {
+    "revision": "8cfdf785d09432c7bd74",
+    "url": "/nubieducation/static/js/runtime-main.af05f851.js"
+  },
+  {
+    "revision": "179f6a778da31168b27b9334f3f2bc26",
+    "url": "/nubieducation/static/media/John_news.179f6a77.jpg"
+  },
+  {
+    "revision": "2a6f848818801f9be3a04d0433e679f4",
+    "url": "/nubieducation/static/media/Sarah_test.2a6f8488.png"
+  },
+  {
+    "revision": "080819b03e1e48b371db3897cb727d07",
+    "url": "/nubieducation/static/media/Tobe_test.080819b0.png"
+  },
+  {
+    "revision": "499816209aab4a876592850fb1cb8dec",
+    "url": "/nubieducation/static/media/Wale_test.49981620.jpg"
+  },
+  {
+    "revision": "45be2dc1c4835e5fd3077163b5446670",
+    "url": "/nubieducation/static/media/Winifred_test.45be2dc1.png"
+  },
+  {
+    "revision": "17502af2c1b5b5bc0cce87d83349e750",
+    "url": "/nubieducation/static/media/Zainab_test.17502af2.png"
+  },
+  {
+    "revision": "c941897616a526a10141351a12ccf609",
+    "url": "/nubieducation/static/media/home_image.c9418976.jpg"
+  },
+  {
+    "revision": "c981745a5e6b250498f0fd462ab2cc0a",
+    "url": "/nubieducation/static/media/home_image1.c981745a.jpg"
+  }
+]);
